682b16ce7161004a0a0242d5a520c600855cb7f1
FWI-95 <devfoxone@gmail.com>
2023-06-26 18:43:26 +0200

    initial commit

---
+++ ClickerGame/src/CookieClicker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CookieClicker;
+
+namespace ClickerGame // Note: actual namespace depends on the project name.
+{
+    internal class CookieClicker
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Inventory inventory = new Inventory();
+
+            
+        }
+    }
+}
---
+++ ClickerGame/src/Helpers.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CookieClicker {
+    internal class Helper{
+        
+
+    }
+
+    internal class Grandma : Helper{
+
+    }
+    internal class Farm : Helper {
+
+    }
+}
